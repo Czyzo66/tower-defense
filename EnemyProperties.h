e99c6467574222ee582315834ccfcb44368273ae
Adam Czyzewski <adamczyzo@protonmail.com>
2018-11-12 14:05:56 +0100

    class storing properties of Enemy types

---
+++ EnemyProperties.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <SFML/System/Vector2.hpp>
+class EnemyProperties
+{
+private:
+	std::string m_name;
+	double m_hp;
+	double m_speed;
+	sf::Vector2f m_size;
+	std::string m_textureFile;
+public:
+	EnemyProperties() {};
+	EnemyProperties(const std::string& name, const double hp, const double speed, const sf::Vector2f& size, const std::string& textureFile) :
+		m_name(name), m_hp(hp), m_speed(speed), m_size(size), m_textureFile(textureFile) {};
+	const std::string& getName() const { return m_name; };
+	const double getHp() const { return m_hp; };
+	const double getSpeed() const { return m_speed; };
+	const sf::Vector2f& getSize() const { return m_size; };
+	const std::string& getTextureFile() const { return m_textureFile; };
+};
+
