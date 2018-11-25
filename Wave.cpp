2a1adc72b6b7e9f5797e850d856e673d2c9ff3ce
Adam Czyzewski <adamczyzo@protonmail.com>
2018-11-25 01:18:13 +0100

    Initial implementation of Wave

--- Wave.cpp
+++ Wave.cpp
@@ -1,2 +1,63 @@
 #include "Wave.h"
 
+void Wave::skipChars(std::ifstream & inf) const
+{
+	char ch = 0;
+	while (ch != '[')
+		inf >> ch;
+}
+
+void Wave::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	for (const Enemy& enemy : m_enemies)
+	{
+		if (enemy.isActive())
+			target.draw(enemy, states);
+	}
+}
+
+int Wave::loadFromFile(std::ifstream & inf, std::vector<Track>* ptrTracks)
+{
+	int trackNum, timeAsMilliseconds;
+	sf::Time time;
+	inf >> trackNum;
+	skipChars(inf);
+	inf >> timeAsMilliseconds;
+	m_startTime = sf::milliseconds(timeAsMilliseconds);
+	skipChars(inf);
+	while (inf.peek() != 'e')
+	{
+		int qty, id;
+		inf >> id;
+		skipChars(inf);
+		inf >> qty;
+		skipChars(inf);
+		for (int i = 0; i < qty; ++i)
+		{
+			m_enemies.push_back(Enemy(id, &ptrTracks->at(trackNum)));
+		}		
+	}
+	skipChars(inf);
+	return 0;
+}
+
+void Wave::release(sf::Clock& clock)
+{
+	if (!m_active && clock.getElapsedTime().asMilliseconds() == m_startTime.asMilliseconds())
+	{
+		m_active = true;
+		for (Enemy& enemy : m_enemies)
+		{
+			enemy.setActive(true);
+			enemy.resetTimer();
+		}
+	}
+	else if (m_active)
+	{
+		for (Enemy& enemy : m_enemies)
+		{
+			enemy.move();
+		}
+	}
+	
+}
--- Wave.h
+++ Wave.h
@@ -1,12 +1,23 @@
 #pragma once
+#include <SFML/System/Time.hpp>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include "Error.h"
 #include "Enemy.h"
 
-class Wave
+class Wave : public sf::Drawable
 {
 private:
 	std::vector<Enemy> m_enemies;
+	sf::Time m_startTime;
+	bool m_active;
+	void skipChars(std::ifstream& inf) const;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 public:
-	
+	Wave(std::ifstream & inf, std::vector<Track>* ptrTracks) : m_active(false) { loadFromFile(inf, ptrTracks); }
+	int loadFromFile(std::ifstream& inf, std::vector<Track>* ptrTracks);
+	const std::vector<Enemy>& getEnemies() const { return m_enemies; }
+	void release(sf::Clock& clock);
 };
 
