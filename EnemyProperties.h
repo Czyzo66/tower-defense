8f90098192a4d5cf32e2ec14b0d30d86330b97d5
Adam Czyzewski <adamczyzo@protonmail.com>
2018-11-25 01:02:41 +0100

    removed unnecessary const in getters

--- EnemyProperties.h
+++ EnemyProperties.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <string>
 #include <SFML/System/Vector2.hpp>
+#include <string>
 class EnemyProperties
 {
 private:
@@ -14,8 +14,8 @@ public:
 	EnemyProperties(const std::string& name, const double hp, const double speed, const sf::Vector2f& size, const std::string& textureFile) :
 		m_name(name), m_hp(hp), m_speed(speed), m_size(size), m_textureFile(textureFile) {};
 	const std::string& getName() const { return m_name; };
-	const double getHp() const { return m_hp; };
-	const double getSpeed() const { return m_speed; };
+	double getHp() const { return m_hp; };
+	double getSpeed() const { return m_speed; };
 	const sf::Vector2f& getSize() const { return m_size; };
 	const std::string& getTextureFile() const { return m_textureFile; };
 };
