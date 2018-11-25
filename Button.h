a8027ded7a28a739c122b7d8be9a45d78fa4498d
Adam Czyzewski <adamczyzo@protonmail.com>
2018-11-25 01:01:14 +0100

    Button is now inheriting from GraphicsEntity

--- Button.h
+++ Button.h
@@ -1,26 +1,22 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <string>
-//#include <vector>
-//#include <cmath>
 #include "Error.h"
+#include "GraphicsEntity.h"
 
-class Button : public sf::Drawable
+class Button : public GraphicsEntity
 {
 private:
-	sf::Texture m_texture;
-	sf::Sprite m_sprite;
 	sf::FloatRect m_boundingBox;
 	bool m_isActive;
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	void init();
 public:
-	int load(const std::string& textureFile);
-	int load(const std::string& textureFile, const sf::Vector2f& position);
-	int load(const std::string& textureFile, const float positionX, const float positionY);
-	void setPosition(const sf::Vector2f& position);
-	void setPosition(const float positionX, const float positionY);
+	Button(const std::string& textureFile) : GraphicsEntity(textureFile) { init(); }
+	Button(const std::string& textureFile, const sf::Vector2f& position) : GraphicsEntity(textureFile, position) { init(); }
+	Button(const std::string& textureFile, const float positionX, const float positionY) : GraphicsEntity(textureFile, positionX, positionY) { init(); }
+	const sf::FloatRect& getBoundingBox() const { return m_boundingBox; }
+	virtual void setPosition(const sf::Vector2f& position);
+	virtual void setPosition(const float positionX, const float positionY);
 	void setActive(bool active);
-	void rescale(float scale);
-	const sf::FloatRect& getBoundingBox() const;
+	void rescale(const float scale);
 };
-
